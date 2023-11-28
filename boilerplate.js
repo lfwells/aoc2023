f408a2a99c9e490328ac617d954a21bfd19e054c
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2023-11-29 01:00:52 +1100

    change from console.time to process.hrtime

--- boilerplate.js
+++ boilerplate.js
@@ -3,11 +3,23 @@ import * as fs from 'fs';
 export default function run(day, f)
 {
     console.log(`--- Day ${day} ---`);
-    console.time("run");
+    var startTime = process.hrtime();
 
     const input = fs.readFileSync("day1.input").toString();
     f(input);
+    for (var i = 0; i < 1000; i++) console.log("haha");
+    
+    var elapsedSeconds = parseHrtimeToSeconds(process.hrtime(startTime));
+    console.log(`--- End Day ${day} in ${elapsedSeconds} seconds---`);
+}
 
-    console.log(`--- End Day ${day} ---`);
-    console.timeEnd("run");
+
+function parseHrtimeToSeconds(hrtime) {
+    var seconds = (hrtime[0] + (hrtime[1] / 1e9)).toFixed(3);
+    return seconds;
+}
+
+function recordRunTime(day, time)
+{
+
 }
