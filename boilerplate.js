427e55d08d69d87fb13f331b909c0f95f4e82500
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2023-11-29 01:05:12 +1100

    record how many times a day is run

--- boilerplate.js
+++ boilerplate.js
@@ -2,7 +2,8 @@ import * as fs from 'fs';
 
 export default function run(day, f)
 {
-    console.log(`--- Day ${day} ---`);
+    var runNumber = recordRun(day);
+    console.log(`--- Day ${day} (Run #${runNumber}) ---`);
     var startTime = process.hrtime();
 
     const input = fs.readFileSync("day1.input").toString();
@@ -10,7 +11,7 @@ export default function run(day, f)
     for (var i = 0; i < 1000; i++) console.log("haha");
     
     var elapsedSeconds = parseHrtimeToSeconds(process.hrtime(startTime));
-    console.log(`--- End Day ${day} in ${elapsedSeconds} seconds---`);
+    console.log(`--- End Day ${day} in ${elapsedSeconds} seconds (Run #${runNumber}) ---`);
 }
 
 
@@ -19,6 +20,28 @@ function parseHrtimeToSeconds(hrtime) {
     return seconds;
 }
 
+function recordRun(day)
+{
+    //load a json from the stats file
+    if (!fs.existsSync("stats.json")) {
+        fs.writeFileSync("stats.json", JSON.stringify({ runs: [] }));
+    }
+    
+    var statsRaw = fs.readFileSync("stats.json").toString();
+    var stats = JSON.parse(statsRaw);
+
+    //add a run to the json
+    var run = {day: day, time: 0, date: new Date()};
+    stats.runs.push(run);
+
+    //save the json back to the stats file
+    statsRaw = JSON.stringify(stats);
+    fs.writeFileSync("stats.json", statsRaw);
+
+    //return the number of times this day has been run
+    return stats.runs.filter(r => r.day == day).length ?? 0;
+
+}
 function recordRunTime(day, time)
 {
 
