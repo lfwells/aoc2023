0dbc9856a710b7c899d8314925c6b277c8005eb2
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2023-12-02 20:14:44 +1100

    day 2 part 1

--- day2.js
+++ day2.js
@@ -1,8 +1,44 @@
 import run from "./boilerplate.js";
+import { sum } from "./utils.js";
+
+let limits = {
+    "red": 12,
+    "green": 13,
+    "blue": 14
+}
 
 run(2, (input) => 
 {
     //code goes here
-    
+    let lines = input.split("\r\n");
+    let games = {};
+    for (var line of lines)
+    {
+        let [game, actions] = line.split(": ");
+        game = game.replace("Game ", "");
+
+        actions = actions.split("; ").map(a => a.split(", ").map(b => b.split(" ")));
+        games[game] = actions;
+    }
+    //console.log({game:JSON.stringify(games[1])});
+
+    let valid = Object.entries(games).map(([game, sets]) => {
+        let valid = true;
+        for (var actions of sets)
+        {
+            for (var action of actions)
+            {
+                let [number, color] = action;
+                console.log({game, color, number});
+                if (limits[color] < number) valid = false;
+            }
+        }
+        return [game, valid];
+    });
+    //console.log({valid});
+
+    let sums = valid.filter(v => v[1]);
+    let s = sum(sums.map(v => parseInt(v[0])));
+    console.log({s});
     
 });
