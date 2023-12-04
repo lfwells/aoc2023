a5dc9751c4d34486427db4932eab304e98e0be32
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2023-12-04 16:10:39 +1100

    day 4 part 1

--- day4.js
+++ day4.js
@@ -1,8 +1,17 @@
 import run from "./boilerplate.js";
+import { parseInts, sum } from "./utils.js";
 
 run(4, (input) => 
 {
     //code goes here
-    
-    
+    var games = input.split("\r\n").map(game => game.split(": ")[1]).map(game => game.split(" | ").map(card => parseInts(card.split(" ").filter(number => number != ""))));
+    console.log(games[0]);
+
+    let numbersWon = games.map(game => game[1].map(ourNumber => game[0].includes(ourNumber)).filter(ourNumber => ourNumber == true).length);
+    console.log({numbersWon});
+
+    let scores = numbersWon.map(count => {
+        return count == 0 ? 0 : Math.pow(2, count-1);
+    });
+    console.log({s:sum(scores)});
 });
