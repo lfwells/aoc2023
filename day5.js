4d3b2fe95101aaf62d2a487667a7928c7c0614df
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2023-12-05 23:23:50 +1100

    day 5 part 2 naive approach

--- day5.js
+++ day5.js
@@ -52,4 +52,18 @@ run(5, (input) =>
 
     let locations = input.seeds.map(seedToLocation);
     console.log(Math.min(...locations));
+
+    //part 2
+    let seeds = input.seeds;
+    let allSeeds = [];
+    for (var i = 0; i < seeds.length; i+=2)
+    {
+        for (var s = seeds[i]; s <= seeds[i]+seeds[i+1]; s++)
+        {
+            allSeeds.push(s);
+        }
+    }
+    console.log({seeds,allSeeds});
+    locations = allSeeds.map(seedToLocation);
+    console.log(Math.min(...locations));
 });
