f1d763577509ef1583c17eadbb241789eb681035
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2023-12-09 21:20:36 +1100

    day 9

--- day9.js
+++ day9.js
@@ -1,8 +1,54 @@
 import run from "./boilerplate.js";
-
+import {parseInts, sum } from "./utils.js";
 run(9, (input) => 
 {
     //code goes here
+    let lines = input.split("\r\n").map(l => l.split(" ")).map(l => l.map(i => parseInt(i)));
+
+    function process(a)
+    {
+        
+        console.log({a});
+        let allZero = a[0] == 0;
+        let newA = [];
+
+        for (var i = 1; i < a.length; i++)
+        {
+            allZero &= a[i] == 0;
+            newA.push(a[i] - a[i - 1]);
+        }
+        if (allZero)
+            return a[a.length - 1];
+
+        return process(newA) + a[a.length - 1];
+    }
+
+    let part1 = sum(lines.map(process));
+    console.log({part1});
+
     
+    function processPart2(a)
+    {
+        
+        console.log({a});
+        let allZero = a[0] == 0;
+        let newA = [];
+
+        for (var i = 1; i < a.length; i++)
+        {
+            allZero &= a[i] == 0;
+            newA.push(a[i] - a[i - 1]);
+        }
+        if (allZero)
+        {
+            return a[0];
+        }
+
+        let r = a[0] - processPart2(newA);
+        return r;
+    }
     
+    let part2 = sum(lines.map(processPart2));
+    console.log({part2});
+
 });
